810530d98ab52908fb8f88d4677fce4eb098aace
Vlad-Pischaeff <pischaeff@gmail.com>
2021-12-15 15:48:37 +0500

    add client list unique key...

--- client/src/components/Clients.svelte
+++ client/src/components/Clients.svelte
@@ -3,15 +3,20 @@
   import Client from './Client.svelte';
   let currentClients;
 
-  $: currentClients = $clients;
+  function clientObject(item) {
+    this.id = {};
+    this.item = item;
+  }
+
+  $: currentClients = $clients.map(n => new clientObject(n));
 </script>
 
 <div class="clients">
   {#if currentClients.length === 0}
     <div class="client-empty">No clients...</div>
   {:else}
-    {#each currentClients as client, i }
-      <Client item={client} index={i} />
+    {#each currentClients as client, i (client.id)}
+      <Client item={client.item} index={i} />
     {/each}
   {/if}
 </div>
