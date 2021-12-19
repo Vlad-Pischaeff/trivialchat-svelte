85cad5bbaedf73befbd2999aabbbd64edcbc1242
Vlad-Pischaeff <pischaeff@gmail.com>
2021-12-19 12:03:10 +0500

    add adapter-static for static deployment...

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,6 +1,14 @@
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+
 const config = {
 	kit: {
+    adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
     host: 'localhost',
 		target: '#svelte',
