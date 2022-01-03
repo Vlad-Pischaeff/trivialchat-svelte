d2db287fb3a8b681f2d60c27df9993d91d8b17c9
Vlad-Pischaeff <pischaeff@gmail.com>
2022-01-03 18:34:15 +0500

    fix comment...

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,91 +1,91 @@
 <script>
-	import { onMount } from 'svelte';
-	import { iconAvatar } from './icons';
-	import { random_id, isEmpty, WS_URL, URL } from './helper';
-	import Header from './Header.svelte';
-	import Message from './Message.svelte';
-	import Footer from './Footer.svelte';
+  import { onMount } from 'svelte';
+  import { iconAvatar } from './icons';
+  import { random_id, isEmpty, WS_URL, URL } from './helper';
+  import Header from './Header.svelte';
+  import Message from './Message.svelte';
+  import Footer from './Footer.svelte';
 
-	let messages = [], inputVal = '', Session, myWorker;
-	let	isReadyServiceWorker = false, isNewSession = false;
+  let messages = [], inputVal = '', Session, myWorker;
+  let	isReadyServiceWorker = false, isNewSession = false;
 
-	const swListener = new BroadcastChannel('swListener');
+  const swListener = new BroadcastChannel('swListener');
 
-	swListener.onmessage = function({data}) {
-		console.log('swListener Received', data);
-		let message = JSON.parse(data);
+  swListener.onmessage = function({data}) {
+    console.log('swListener Received', data);
+    let message = JSON.parse(data);
 
-		if (message.svc) {
-			Session.online = /ONLINE/i.test(message.svc);
-			saveSession();
-		}
+    if (message.svc) {
+      Session.online = /ONLINE/i.test(message.svc);
+      saveSession();
+    }
 
-		if (message.wsState) {
-			//...webSocket state messages
-			console.log('webSocket state...', message.wsState);
-		}
+    if (message.wsState) {
+      //...webSocket state messages
+      console.log('webSocket state...', message.wsState);
+    }
 
-		if (message.wsUser) {
-			//...restore UserId
-			console.log('webSocket user...', message.wsUser);
-			Session.userID = message.wsUser;
-		} 
-		
-		if (message.to) {
-			//...WebSocket clients messages
-			messages = [ ...messages, message];
-		}
-	};
+    if (message.wsUser) {
+      //...restore UserId
+      console.log('webSocket user...', message.wsUser);
+      Session.userID = message.wsUser;
+    } 
 
-	const sendMessage = (val) => {
-		let message = new MessageObj(val)
-		messages = [ ...messages, message];
-		let swMessage = new innerMessageObj('post', message);
-		myWorker.postMessage(JSON.stringify(swMessage));
-	}
+    if (message.to) {
+      //...WebSocket clients messages
+      messages = [ ...messages, message];
+    }
+  };
 
-	function MessageObj(msg) {
-		this.from = Session.userID;
-		this.msg = msg;
-		this.date = Date.now();
-	}
+  const sendMessage = (val) => {
+    let message = new MessageObj(val)
+    messages = [ ...messages, message];
+    let swMessage = new innerMessageObj('post', message);
+    myWorker.postMessage(JSON.stringify(swMessage));
+  }
 
-	function innerMessageObj(type, msg, userId) {
-		this.type = type;
-		this.msg = msg;
-		this.userId = userId;
-	}
+  function MessageObj(msg) {
+    this.from = Session.userID;
+    this.msg = msg;
+    this.date = Date.now();
+  }
 
-	const saveMessages = () => {
-		if (Session) { 
-			Session.userMSGS = messages;
-			saveSession();
-		}
-	}
+  function innerMessageObj(type, msg, userId) {
+    this.type = type;
+    this.msg = msg;
+    this.userId = userId;
+  }
 
-	const saveSession = () => {
-		sessionStorage.setItem('tchat', JSON.stringify(Session));
-	}
+  const saveMessages = () => {
+    if (Session) { 
+      Session.userMSGS = messages;
+      saveSession();
+    }
+  }
 
-	$: if (messages.length !== 0) saveMessages();
+  const saveSession = () => {
+    sessionStorage.setItem('tchat', JSON.stringify(Session));
+  }
 
-	$: if (isReadyServiceWorker && isNewSession) {
-			let innerMsg = new innerMessageObj('init', `${WS_URL}?userName=${Session.userID}&userHost=${Session.userHOST}`, `${Session.userID}`);
-			myWorker.postMessage(JSON.stringify(innerMsg));
-			console.log('swState init...', innerMsg);
-		}
+  $: if (messages.length !== 0) saveMessages();
 
-	onMount(async () => {
-		Session = JSON.parse(sessionStorage.getItem('tchat')) || {};
-		isNewSession = isEmpty(Session) ? true : false;
-		/**
-		 * restore the user's session from sessionStorage, 
-		 * if there is nothing there, we get the data from the server
-		 */
+  $: if (isReadyServiceWorker && isNewSession) {
+      let innerMsg = new innerMessageObj('init', `${WS_URL}?userName=${Session.userID}&userHost=${Session.userHOST}`, `${Session.userID}`);
+      myWorker.postMessage(JSON.stringify(innerMsg));
+      console.log('swState init...', innerMsg);
+    }
+
+  onMount(async () => {
+    Session = JSON.parse(sessionStorage.getItem('tchat')) || {};
+    isNewSession = isEmpty(Session) ? true : false;
+    /**
+     * restore the user's session from sessionStorage, 
+     * if there is nothing there, we get the data from the server
+     */
     if (isNewSession) {
 
-      if (!Session.userID) Session.userID = random_id();
-			Session.online = false;													// ...operator is OFFLINE by default
+    if (!Session.userID) Session.userID = random_id();
+      Session.online = false;													// ...operator is OFFLINE by default
 
       let url = (window.location != window.parent.location)
         ? document.referrer         									// ---- https://tele.scope.cf
@@ -94,80 +94,78 @@
 			
       let response = await fetch(`${URL}/api/auth/usersite/${Session.userHOST}`)
                             .then(response => response.json())
-														.catch(e => e);
+                            .catch(e => e);
 
       if (!response.message) {
-				Session.userTitle = response.title
-					? response.title
-					: 'FAKE corporation.';
-				Session.userDesc = response.desc
-					? response.desc
-					: 'I\'am Your online Manager';
+        Session.userTitle = response.title
+          ? response.title
+          : 'FAKE corporation.';
+        Session.userDesc = response.desc
+          ? response.desc
+          : 'I\'am Your online Manager';
         Session.userAvatar = response.avatar
-					? response.avatar
-					:	iconAvatar;
-				Session.userGreeting = response.greeting
-					? response.greeting
-					: "Hello. What can I help You ?...";
+          ? response.avatar
+          :	iconAvatar;
+        Session.userGreeting = response.greeting
+          ? response.greeting
+          : "Hello. What can I help You ?...";
       }
 
       Session.userMSGS = [{ to: 'me', msg: Session.userGreeting, date: Date.now() }];
-			messages = Session.userMSGS;
+      messages = Session.userMSGS;
 
-			saveSession();
+      saveSession();
 
-		} else {
-			messages = Session.userMSGS;
-		}
-		/**
-		 * set serviceWorker if it does not exist or activate it if it does
-		 */
-		if (navigator.serviceWorker.controller) {
-			myWorker = navigator.serviceWorker.controller;
-			console.log('is already has controller...', myWorker.state);
-			isReadyServiceWorker = true;
-		} else {
-			navigator.serviceWorker
-				.register('websocket-worker.js')
-				.then((registration) => {
-					if (registration.installing) {
-						myWorker = registration.installing;
-					} else if (registration.waiting) {
-						myWorker = registration.waiting;
-					} else if (registration.active) {
-						myWorker = registration.active;
-					}
+    } else {
+      messages = Session.userMSGS;
+    }
+    /**
+     * set serviceWorker if it does not exist or activate it if it does
+     */
+    if (navigator.serviceWorker.controller) {
+      myWorker = navigator.serviceWorker.controller;
+      isReadyServiceWorker = true;
+    } else {
+      navigator.serviceWorker
+        .register('websocket-worker.js')
+        .then((registration) => {
+          if (registration.installing) {
+            myWorker = registration.installing;
+          } else if (registration.waiting) {
+            myWorker = registration.waiting;
+          } else if (registration.active) {
+            myWorker = registration.active;
+          }
 
-					if (myWorker) {
-						myWorker.addEventListener('statechange', e => {
-							console.log('else not yet has controller...', myWorker.state);
-							if (e.target.state === 'activated') {
-								isReadyServiceWorker = true;
-							}
-						});
-					}
-				});
-		}
-		/**
-		 * make queries for the cached empty file so that the sarviceWorker is always active
-		 */
-		let timerId = setInterval(() => { fetch('/ping.txt')}, 20000);
-		return () => clearInterval(timerId);
+          if (myWorker) {
+            myWorker.addEventListener('statechange', e => {
+              if (e.target.state === 'activated') {
+                isReadyServiceWorker = true;
+              }
+            });
+          }
+        });
+    }
+    /**
+     * make queries for the cached empty file so that the sarviceWorker is always active
+     */
+    let timerId = setInterval(() => { fetch('/ping.txt')}, 20000);
+    return () => clearInterval(timerId);
 
-	});
+  });
 
 </script>
 
 <main class="cp" id="App">
-	<Header Session={Session} />
-	<section class="cp_body">
-		<div class="chat_field">
-			{#if messages.length !== 0}
-				{#each messages as message }
-					<Message message={message} />
-				{/each}
-			{/if}
-		</div>
-	</section>
-	<Footer sendMessage={sendMessage} inputVal={inputVal} />
+  <Header Session={Session} />
+  <section class="cp_body">
+    <div class="chat_field">
+      {#if messages.length !== 0}
+        {#each messages as message }
+          <Message message={message} />
+        {/each}
+      {/if}
+    </div>
+  </section>
+  <Footer sendMessage={sendMessage} inputVal={inputVal} />
 </main>
