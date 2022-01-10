fe3b97530b06ba583d7c68c00d791c9aae865fd2
Vlad-Pischaeff <pischaeff@gmail.com>
2022-01-10 11:32:09 +0500

    add send mail icon...

--- client/src/Header.svelte
+++ client/src/Header.svelte
@@ -1,9 +1,10 @@
 <script>
+  import { iconMail } from './icons';
   export let Session;
 </script>
 
 <section class="cp_logo">
-  <a href="https://tchat.scope.cf">trivial chat &copy;</a>
+  <a href="https://tchat.scope.cf" target="_blank" rel="noopener noreferrer">trivial chat &copy;</a>
 </section>
 <section class="cp_header">
   <picture class="cp_header-avatar">
@@ -14,4 +15,13 @@
     <div class="cp_header-card-1">{Session?.userTitle}</div>
     <div class="cp_header-card-2">{Session?.userDesc}</div>
   </div>
+  <img class="w-icon" src={iconMail} alt="mail" title="temporarily unavailable">
 </section>
+
+<style>
+  .w-icon {
+    align-self: end;
+    margin: .5rem;
+    height: 1.5rem;
+  }
+</style>
